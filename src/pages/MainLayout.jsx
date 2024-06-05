8a1b97b54a2adde64becab50aaeab0cdba267b04
Ativie Favour <ativiefavour03@gmail.com>
2024-06-05 19:05:55 +0100

    Routes configuration

--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -1,12 +1,22 @@
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import { Outlet } from 'react-router-dom'
+import { Box } from "@mui/material";
+
 
 const MainLayout = () => {
   return (
     <>
     <Navbar />
+    <Box 
+    overFlow="hidden"
+    flexGrow={1}
+    >
     <Outlet/>
+
+    </Box>
+
+    
     <Footer/>
     
     </>
