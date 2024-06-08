5a22b72ea555258fd5f2dd79402fc0dac00dfd9f
zucm709 <zuckm709@gmail.com>
2024-06-08 18:47:33 +0100

    Home Hero Created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,10 @@ const Navbar = () => {
   return (
     <div className='tw-absolute tw-w-full tw-z-10'>
             <div  className="tw-flex flex-row tw-items-center tw-justify-evenly tw-font-bold tw-text-white">
-                <div className='tw-flex flex-row tw-items-center'>
+                <Link to={'/'} className='tw-flex flex-row tw-items-center'>
                 <img src="/icons/harv.png" alt="harvesta" />
                 <p>Harvesta</p>
-                </div>
+                </Link>
                     <div className='tw-flex flex-row tw-gap-8'>
                             <Link to={'/home'}>Home</Link>
                             <Link to={'/about'}>About</Link>
