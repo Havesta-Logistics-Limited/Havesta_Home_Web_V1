7279b9b285ebea9126685c3d289fba07f9e64ae2
zucm709 <zuckm709@gmail.com>
2024-06-13 11:58:42 +0100

    landing page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,15 @@ import navRoutes from '../routes/nav.routes'
 
 const Navbar = () => {
   return (
-    <div>
-            <div  className="flex flex-row items-center justify-evenly  font-bold ">
-                <div className='flex flex-row items-center'>
-                <img src="/icons/harv.png" alt="harvesta" />
-                <p>Harvesta</p>
-                </div>
+    <div className='absolute  z-10 w-full'>
+            <div  className="flex flex-row items-center justify-evenly text-white  mt-2  font-bold ">
+                <Link to={'/'} className='flex flex-row items-center'>
+                <img src="/icons/harv.png" alt="harvesta" width={'25px'} height={'11,13'} />
+                <p className='ml-1'>Harvesta</p>
+                </Link>
                     <div className='flex flex-row gap-8'>
                             {navRoutes.map((item, index)=>(
-                                <Link to={item.path} key={index}>{item.name}</Link>
+                                <Link to={item.path} key={index} className='hover:bg-harvestaLightGreen px-2 rounded-md'>{item.name}</Link>
                             ))}
                     </div>
                     <Link>
--- src/components/landing/Hero.jsx
+++ src/components/landing/Hero.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 const Hero = () => {
   return (
     <div>
-        <img src="/icons/Hero.svg" alt="harvesta" className='tw-relative tw-mb-11'/>
-        <div className='tw-absolute tw-top-40 tw-text-white tw-ml-48'>
-            <h2 className='tw-text-2xl tw-font-bold'>Find Your Healthy</h2>
-            <h2 className='tw-text-2xl tw-text-harvestaYellow tw-font-bold'>Groceries</h2>
+        <img src="/icons/Hero.png" alt="harvesta" className='relative'/>
+        <div className='absolute top-40 text-white ml-48'>
+            <h2 className='text-2xl font-bold'>Find Your Healthy</h2>
+            <h2 className='text-2xl text-harvestaYellow font-bold'>Groceries</h2>
             <br />
-            <p className='tw-text-xs'>Fresh fruits & veggies, grains, Tubers and More!</p>
+            <p className='text-xs'>Fresh fruits & veggies, grains, Tubers and More!</p>
             <br />
-            <div className='tw-flex tw-flex-row tw-items-center'>
-                <img src="/icons/Vector.svg" alt=""  className='tw-bg-harvestaGreen tw-py-1 tw-px-1.5 tw-rounded-s'/>
-                <input type="text" placeholder='search for your groceries...'  className='tw-py-1.5 tw-outline-0 tw-w-80 tw-px-2.5 tw-rounded-e tw-text-gray-500 tw-text-xs'/>
+            <div className='flex flex-row items-center'>
+                <img src="/icons/Vector.svg" alt=""  className='bg-harvestaLightGreen py-1 px-1.5 rounded-s'/>
+                <input type="text" placeholder='search for your groceries...'  className='py-1.5 outline-0 w-80 px-2.5 rounded-e text-gray-500 text-xs'/>
             </div>
         </div>
     </div>
