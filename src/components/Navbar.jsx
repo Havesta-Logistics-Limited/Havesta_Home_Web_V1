c474f85b5b462912ca71a2ca053095b262d29cf3
Ativie Favour <ativiefavour03@gmail.com>
2024-06-21 17:36:41 +0100

    Homepage revamp

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,76 +1,45 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import navRoutes from '../routes/nav.routes';
+import { Link } from "react-router-dom";
+import navRoutes from "../routes/nav.routes";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
 const Navbar = () => {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [partnerLabel, setPartnerLabel] = useState('Partner');
-
-  const toggleDropdown = () => {
-    setDropdownOpen(!dropdownOpen);
-  };
-
-  const closeDropdown = () => {
-    setDropdownOpen(false);
-  };
-
-  const handleSubmenuClick = (submenuName, submenuPath) => {
-    setPartnerLabel(submenuName);
-    closeDropdown();
-  };
+  
 
   return (
-    <div className='absolute z-10 w-full'>
+    <div className="absolute z-10 w-full">
       <div className="flex flex-row items-center justify-evenly text-white mt-8 font-primary">
-        <Link to={'/'} className='flex flex-row items-center'>
-          <img src="/icons/harv.png" alt="harvesta" width={'25px'} height={'11,13'} className='' />
-          <p className='ml-1 '>Harvesta</p>
+        <Link to={"/"} className="flex flex-row items-center">
+          <img
+            src="/icons/harv.png"
+            alt="harvesta"
+            width={"25px"}
+            height={"11.13px"}
+            className=""
+          />
+          <p className="ml-1">Harvesta</p>
         </Link>
-        <div className='flex flex-row gap-2 relative'>
+        <div className="flex flex-row gap-5 relative">
           {navRoutes.map((item, index) => (
-            <div key={index} className='relative'>
-              {item.submenu ? (
-                <div className='relative'>
-                  <button
-                    onClick={toggleDropdown}
-                    className='hover:bg-harvestaLightGreen px-2 rounded-md flex items-center'
-                  >
-                    {partnerLabel}
-                    <svg className="ml-1 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path>
-                    </svg>
-                  </button>
-                  {dropdownOpen && (
-                    <div className='absolute w-36 rounded-md'>
-                      <div className='py-1' role='menu' aria-orientation='vertical' aria-labelledby='options-menu'>
-                        {item.submenu.map((submenuItem, subIndex) => (
-                          <Link
-                            key={subIndex}
-                            to={submenuItem.path}
-                            className='block ml-2 text-sm text-white rounded-md text-center hover:bg-harvestaLightGreen'
-                            onClick={() => handleSubmenuClick(submenuItem.name, submenuItem.path)}
-                          >
-                            {submenuItem.name}
-                          </Link>
-                        ))}
-                      </div>
-                    </div>
-                  )}
-                </div>
-              ) : (
-                <Link
-                  to={item.path}
-                  className='hover:bg-harvestaLightGreen px-2 rounded-md'
-                >
-                  {item.name}
-                </Link>
-              )}
+            <div key={index} className="relative">
+              <Link
+                to={item.path}
+                className="text-sm px-2 rounded-md hover:bg-harvestaLightGreen hover:p-2 transition-all"
+              >
+                {item.name}
+              </Link>
             </div>
           ))}
         </div>
-        <Link to={'/marketplace'}>
-          <button className='bg-harvestaYellow px-4 py-1 w-28 rounded-full text-white'>Market</button>
-        </Link>
+        <div className="relative">
+      <Link to={"/marketplace"} className="relative">
+        <span className="bg-harvestaYellow p-2 w-12 h-12 rounded-full text-white flex items-center justify-center transition-all">
+          <AddShoppingCartIcon/>
+        </span>
+        <span className="bg-harvestaYellow p-2 w-12 h-12 rounded-full text-white flex items-center justify-center hover:w-20 absolute top-0 left-0 transition-all">
+        <AddShoppingCartIcon/>
+        </span>
+      </Link>
+    </div>
       </div>
     </div>
   );
--- src/components/landing/Reviews.jsx
+++ src/components/landing/Reviews.jsx
@@ -1,82 +1,108 @@
-
-import React, { useState } from 'react';
-import ReviewsRoutes from '../../routes/reviews.routes';
+import React, { useState } from "react";
+import ReviewsRoutes from "../../routes/reviews.routes";
 
 const Reviews = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   const handlePrevClick = () => {
-    const prevSlide = currentSlide === 0 ? ReviewsRoutes.length - 1 : currentSlide - 1;
+    const prevSlide =
+      currentSlide === 0 ? ReviewsRoutes.length - 1 : currentSlide - 1;
     setCurrentSlide(prevSlide);
   };
 
   const handleNextClick = () => {
-    const nextSlide = currentSlide === ReviewsRoutes.length - 1 ? 0 : currentSlide + 1;
+    const nextSlide =
+      currentSlide === ReviewsRoutes.length - 1 ? 0 : currentSlide + 1;
     setCurrentSlide(nextSlide);
   };
 
   return (
-    <div className='justify-center p-3 font-primary'>
-      <h1 className='text-center font-bold text-3xl items-center'>
+    <div className="justify-center p-3 font-primary">
+      <h1 className="text-center font-bold text-3xl items-center">
         Customer Reviews
       </h1>
-      <p className='text-center font-primary items-center p-2'>
-        We are proud of our accomplishments. We will keep <br /> delivering excellence and satisfaction.
+      <p className="text-center font-primary items-center p-2 mt-4 text-gray-700 text-sm">
+        We are proud of our accomplishments. We will keep <br /> delivering
+        excellence and satisfaction.
       </p>
       <br />
-      <div className='relative w-full flex justify-center items-center'>
-        <div className='w-7/12 overflow-hidden'>
-          <div className='flex transition-transform duration-300' style={{ transform: `translateX(-${currentSlide * 100}%)` }}>
+      <div className="relative w-full flex justify-center items-center">
+        <div className="w-[700px] overflow-hidden shadow-lg">
+          <div
+            className="flex transition-transform duration-300"
+            style={{ transform: `translateX(-${currentSlide * 100}%)` }}
+          >
             {ReviewsRoutes.map((review, index) => (
-              <div key={index} className='flex-shrink-0 w-full'>
-                <div className='flex flex-row items-center bg-harvestaYellow p-2 rounded-md w-full mr-8'>
-                  <div className='mr-8'>
-                    <img src={review.image} alt="harvesta" width={'230px'} />
+              <div key={index} className="flex-shrink-0 w-full">
+                <div className="flex flex-row items-center bg-harvestaYellow p-2 rounded-md w-full mr-8">
+                  <div className="">
+                    <img
+                      src="https://res.cloudinary.com/dtc89xi2r/image/upload/v1717957973/Image_mfqohu.png"
+                      alt="harvesta"
+                      width={"590px"}
+                    />
                   </div>
-                  <div className='flex-1 px-4'>
-                    <img src="/icons/Shape.svg" alt="harvesta" className='self-center ml-36 p-2'/>
-                    <p className='text-center items-center' style={{
-                      display: '-webkit-box',
-                      WebkitBoxOrient: 'vertical',
-                      overflow: 'hidden',
-                      WebkitLineClamp: 3
-                    }}>
+                  <div className="grid grid-col-1  justify-items-center py-4">
+                    <img
+                      src="/icons/Shape.svg"
+                      alt="harvesta"
+                      className="p-2 mt-10"
+                    />
+                    <p
+                      className="text-center text-sm font-bold leading-8 p-5"
+                      style={{
+                        display: "-webkit-box",
+                        WebkitBoxOrient: "vertical",
+                        overflow: "hidden",
+                        WebkitLineClamp: 3,
+                      }}
+                    >
                       {review.text}
                     </p>
                     <br />
-                    <h2 className='text-center items-center'>{review.name}</h2>
-                    <h3 className='text-center items-center'>{review.title}</h3>
+                    <h2 className="text-center font-bold">{review.name}</h2>
+                    <h3 className="text-center text-primary text-sm font-bold">
+                      {review.title}
+                    </h3>
                   </div>
                 </div>
               </div>
             ))}
           </div>
         </div>
-        <button onClick={handlePrevClick} className='absolute left-56 ml-4 py-4 px-3 bg-harvestaDarkGreen rounded-full hover:bg-harvestaDarkGreen'>
+        <button
+          onClick={handlePrevClick}
+          className="absolute left-0 ml-[-30px] py-4 px-3 bg-harvestaDarkGreen rounded-full hover:bg-harvestaDarkGreen"
+        >
           <img src="/icons/tail-left.png" alt="harvesta" />
         </button>
-        <button onClick={handleNextClick} className='absolute right-56 mr-4 py-4 px-3 bg-harvestaDarkGreen rounded-full hover:bg-harvestaDarkGreen'>
+        <button
+          onClick={handleNextClick}
+          className="absolute right-0 mr-[-30px]  py-4 px-3 bg-harvestaDarkGreen rounded-full hover:bg-harvestaDarkGreen"
+        >
           <img src="/icons/tail-right.png" alt="harvesta" />
         </button>
       </div>
-      <br /><br /> <br />
-      <div className='flex flex-row  justify-evenly'>
-        <div className='items-center flex flex-col'>
-          <h1 className='text-6xl text-harvestaDarkGreen'>1M+</h1>
-          <h3>Monthly Customer Visit</h3>
+      <br />
+      <br /> <br />
+      <div className="flex flex-row justify-evenly gap-[40px]">
+        <div className="items-center flex flex-col">
+          <h1 className="text-6xl text-harvestaDarkGreen font-bold">1M+</h1>
+          <h3 className="font-semibold">Monthly Customer Visit</h3>
         </div>
-        <div className='flex flex-col items-center'>
-          <h1 className='text-6xl text-harvestaDarkGreen'>92%</h1>
-          <h3>Customer Satisfaction Rate</h3>
+        <div className="flex flex-col items-center">
+          <h1 className="text-6xl text-harvestaDarkGreen font-bold">92%</h1>
+          <h3 className="font-semibold">Customer Satisfaction Rate</h3>
         </div>
-        <div className='items-center flex flex-col'>
-          <h1 className='text-6xl text-harvestaDarkGreen'>4.9</h1>
-          <h3>Average Customer Ratings</h3>
+        <div className="items-center flex flex-col">
+          <h1 className="text-6xl text-harvestaDarkGreen font-bold">4.9</h1>
+          <h3 className="font-semibold">Average Customer Ratings</h3>
         </div>
       </div>
-      <br /><br /> <br />
+      <br />
+      <br /> <br />
     </div>
   );
-}
+};
 
 export default Reviews;
