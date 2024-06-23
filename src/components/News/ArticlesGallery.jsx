a6073f84f582949894fcf6fc65648e8360be8c27
ImoDavid <emperorjagz@gmail.com>
2024-06-23 17:32:13 +0100

    feat:singleNews page in progress.

--- src/components/News/ArticlesGallery.jsx
+++ src/components/News/ArticlesGallery.jsx
@@ -9,87 +9,90 @@ import {
   Typography,
 } from "@mui/material";
 import { colors } from "../../utils/globals";
-
-const GridContainer = styled(Box)({
-  display: "grid",
-  gridTemplateColumns: `repeat(auto-fill, minmax(200px, 1fr))`,
-  gridGap: "30px",
-  alignItems: "stretch",
-
-  "@media screen and (max-width: 1200px)": {
-    gridGap: "14px",
-    gridTemplateColumns: `repeat(auto-fill, minmax(200px, 1fr))`,
-  },
-});
+import { Link } from "react-router-dom";
 
 const StyledGridElements = styled(Box)({
   width: "100%",
 });
 
-const ArticlesGallery = ({ data }) => {
+const ArticlesGallery = ({ data, gridWidth = "200px" }) => {
+  const GridContainer = styled(Box)({
+    display: "grid",
+    gridTemplateColumns: `repeat(auto-fill, minmax(${gridWidth}, 1fr))`,
+    gridGap: "30px",
+    alignItems: "stretch",
+
+    "@media screen and (max-width: 1200px)": {
+      gridGap: "14px",
+      gridTemplateColumns: `repeat(auto-fill, minmax(200px, 1fr))`,
+    },
+  });
+
   return (
     <Box marginTop={[6, 6]} marginBottom={[6, 8]}>
       <GridContainer>
         {data.map((ele, index) => (
           <StyledGridElements key={index}>
-            <Card
-              sx={{
-                backgroundColor: `${colors.harvestaLightGreen}`,
-                p: 1,
-                borderRadius: "25px",
-                border: "1px solid rgba(0, 0, 0, 0.1)",
-                boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.05)",
-                borderColor: `grey`,
-                ":hover": {
-                  backgroundColor: `${colors.primary}`,
-                },
-              }}
-              elevation={0}
-            >
-              <CardMedia
-                component="img"
-                alt=""
-                height="200"
-                image={ele.img}
-                sx={{ objectFit: "cover", borderRadius: "25px 25px 0 0" }}
-              />
-              <CardContent sx={{ paddingX: 0 }}>
-                {ele.des && (
-                  <Stack direction="column">
-                    <Typography
-                      fontWeight="500"
-                      paddingRight={[1, 5]}
-                      mb
-                      color={colors.WHITE}
-                      textTransform={"capitalize"}
-                      fontFamily={"Plus Jakarta Sans"}
-                    >
-                      {ele.des}
-                    </Typography>
-                    <Stack
-                      direction="row"
-                      justifyContent="space-between"
-                      alignItems="center"
-                    >
+            <Link to={`/news/${ele.des}`}>
+              <Card
+                sx={{
+                  backgroundColor: `${colors.harvestaLightGreen}`,
+                  p: 1,
+                  borderRadius: "25px",
+                  border: "1px solid rgba(0, 0, 0, 0.1)",
+                  boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.05)",
+                  borderColor: `grey`,
+                  ":hover": {
+                    backgroundColor: `${colors.primary}`,
+                  },
+                }}
+                elevation={0}
+              >
+                <CardMedia
+                  component="img"
+                  alt=""
+                  height="200"
+                  image={ele.img}
+                  sx={{ objectFit: "cover", borderRadius: "25px 25px 0 0" }}
+                />
+                <CardContent sx={{ paddingX: 0 }}>
+                  {ele.des && (
+                    <Stack direction="column">
                       <Typography
-                        fontSize="0.8rem"
+                        fontWeight="500"
+                        paddingRight={[1, 5]}
+                        mb
                         color={colors.WHITE}
+                        textTransform={"capitalize"}
                         fontFamily={"Plus Jakarta Sans"}
                       >
-                        Lorem ipsum dolor sit amet consectetur, adipisicing
-                        elit. Minus accusantium molestias rem illo
-                        exercitationem quaerat consectetur. Iure aliquid facere
-                        fugit debitis odio commodi totam repellat ducimus,
-                        beatae itaque non culpa!
+                        {ele.des}
                       </Typography>
-                      {/* <Avatar sx={{ background: `${colors.harvestaYellow}` }}>
+                      <Stack
+                        direction="row"
+                        justifyContent="space-between"
+                        alignItems="center"
+                      >
+                        <Typography
+                          fontSize="0.8rem"
+                          color={colors.WHITE}
+                          fontFamily={"Plus Jakarta Sans"}
+                        >
+                          Lorem ipsum dolor sit amet consectetur, adipisicing
+                          elit. Minus accusantium molestias rem illo
+                          exercitationem quaerat consectetur. Iure aliquid
+                          facere fugit debitis odio commodi totam repellat
+                          ducimus, beatae itaque non culpa!
+                        </Typography>
+                        {/* <Avatar sx={{ background: `${colors.harvestaYellow}` }}>
                         <IoIosArrowForward color={colors.primary} />
                       </Avatar> */}
+                      </Stack>
                     </Stack>
-                  </Stack>
-                )}
-              </CardContent>
-            </Card>
+                  )}
+                </CardContent>
+              </Card>
+            </Link>
           </StyledGridElements>
         ))}
       </GridContainer>
