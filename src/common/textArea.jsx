faa7a6aebb3c4b4bce34052e48db30c8bfe812ff
ImoDavid <emperorjagz@gmail.com>
2024-07-30 13:12:21 +0100

    feat: contact page done.

---
+++ src/common/textArea.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { ErrorMessage, useField, Field } from 'formik';
+import { Box, Typography, styled } from '@mui/material';
+
+const StyledFormGroup = styled(Box)({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  marginBottom: '1rem',
+});
+
+const StyledInput = styled(Field)({
+  display: 'block',
+  width: '100%',
+  height: '10rem',
+  padding: '.5rem',
+  border: '1px solid grey',
+  borderRadius: '15px',
+  '&.is-invalid': {
+    border: '1px solid red',
+  },
+});
+
+const StyledErrorMessage = styled(ErrorMessage)({
+  color: 'red',
+  marginTop: '.25rem',
+});
+
+const TextArea = ({ label, touched, ...props }) => {
+  const [field, meta] = useField(props);
+
+  return (
+    <StyledFormGroup>
+      <Typography component="label" htmlFor={field.name} gutterBottom>
+        {label}
+      </Typography>
+      <StyledInput
+        {...field}
+        {...props}
+        as="textarea"
+        className={meta.touched && meta.error ? 'is-invalid' : ''}
+        name={field.name}
+        id={field.name}
+      />
+      {meta.touched && meta.error ? (
+        <StyledErrorMessage name={field.name} component="div" />
+      ) : null}
+    </StyledFormGroup>
+  );
+};
+
+export default TextArea;
---
+++ src/common/textInput.jsx
@@ -0,0 +1,88 @@
+import { Stack, Box, styled, Typography, InputBase } from '@mui/material';
+const StyledFormGroup = styled(Box)({
+  display: 'flex',
+  flex: ' 0 0 auto',
+  flexFlow: 'column wrap',
+  alignItems: 'start',
+  marginBottom: 0,
+});
+
+const StyledInput = styled(InputBase)({
+  borderBottom: '1px solid #808080',
+  background: 'ffffff',
+
+  '&:focus': {
+    color: '#495057', // Adjust as necessary
+    backgroundColor: '#fff', // Adjust as necessary
+    borderColor: 'transparent', // Ensure other borders don't appear
+    borderBottomColor: '#80bdff', // Change the bottom border color on focus
+    outline: 0,
+    boxShadow: '0 0 0 0.2rem rgba(0, 123, 255, 0.25)', // Adjust as necessary
+  },
+});
+
+const TextInput = ({ name, label }) => {
+  return (
+    <>
+      <StyledFormGroup>
+        <Typography htmlFor={name} gutterBottom fontWeight={500}>
+          {label}
+        </Typography>
+        <StyledInput name={name} id={name} />
+      </StyledFormGroup>
+    </>
+  );
+};
+
+export default TextInput;
+// const TextInput = ({
+//     label,
+//     error,
+//     required,
+//     onChange,
+//     placeholder,
+//     showLabel,
+//     inPutClassName,
+//     icon,
+//     defaultValue,
+//     value,
+//     showIcon,
+//     inputContainerClassName,
+//     type = "text",
+//     disabled = false,
+//     min,
+//     max
+//   }) => {
+//     return (
+//       <div className="tw-w-full tw-flex tw-flex-col tw-items-start tw-justify-start tw-gap-2">
+//         {showLabel && (
+//           <label className="tw-flex tw-items-center tw-justify-center tw-gap-1 tw-capitalize">
+//             <span className="tw-font-medium tw-text-sm tw-text-gray-900">{label}</span>
+//             {required && <span className="tw-font-medium tw-text-sm tw-text-red-500">*</span>}
+//           </label>
+//         )}
+//         <div
+//           className={`${inputContainerClassName} tw-w-full tw-flex tw-items-center tw-justify-start tw-border tw-border-gray-300 tw-bg-gray-50 tw-gap-2.5 tw-rounded-lg`}>
+//           {showIcon && <div>{icon}</div>}
+//           <input
+//             type={type}
+//             value={value}
+//             defaultValue={defaultValue}
+//             onKeyDown={(e) => {
+//               const invalidChars = ["@", "$", "#", "%", "&", "^", "*", "!", "~", "`"];
+//               if (invalidChars.includes(e.key) && type === "text") {
+//                 e.preventDefault(); // Prevent the input of invalid characters
+//               }
+//             }}
+//             disabled={disabled}
+//             onChange={onChange}
+//             className={`${inPutClassName} tw-bg-gray-50 tw-rounded-lg tw-w-full tw-shadow-none focus:tw-outline-none`}
+//             placeholder={placeholder}
+//             min={min}
+//             max={max}
+//           />
+//         </div>
+//         {error && <small className="tw-text-red-500 ">{error}</small>}
+//       </div>
+//     );
+//   };
